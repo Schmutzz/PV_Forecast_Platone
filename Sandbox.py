b1a421a21e29b18c12cfb8fe2610d306c7bdd460
Max <maximilian.boje@gmail.com>
2022-11-06 18:38:05 +0100

    hihi dwd api

--- Sandbox.py
+++ Sandbox.py
@@ -1,3 +1,16 @@
+import requests
+import json
+
+id = 'E438'
+call = 'stationOverview?stationIds='
+url1 = 'https://s3.eu-central-1.amazonaws.com/app-prod-static.warnwetter.de/v16/'
+url2 = 'https://app-prod-ws.warnwetter.de/v30/'
+url = url2 + call + id
+print(url)
+
+json_obj = json.loads(requests.get(url=url).text)
+print(json_obj)
+
 print("100")
 print("0")
 print("14")

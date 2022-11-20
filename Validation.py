56d8af961736e799f0db561347f42f206f539a64
Max <maximilian.boje@gmail.com>
2022-11-20 22:59:47 +0100

    data import to sql db

--- Validation.py
+++ Validation.py
@@ -1,29 +1,32 @@
 import pandas as pd
 from sklearn.metrics import r2_score
 
-class Validation():
-    def __init__(self, df):
-        self.df=df
 
-    def calcR2_score(self, df = None, realValCol=None, forecastCol=None):
-        #input
-        if type(df)==type(None):
-            tempDf=self.df
+class Validation:
+    def __init__(self, df):
+        self.df = df
+
+    def calcR2_score(self, df=None, realValCol=None, forecastCol=None):
+        # input
+        if type(df) == type(None):
+            tempDf = self.df
         else:
-            tempDf=df
-        
-        #function
+            tempDf = df
+
+        # function
         # series need to be of same length
         tempDf['R2'] = r2_score(tempDf[realValCol], tempDf[forecastCol])
 
-        #output
-        if type(df)==type(None):
-            self.df=tempDf
+        # output
+        if type(df) == type(None):
+            self.df = tempDf
         else:
             return tempDf
 
-data={'forecast':[0, 1, 2, 3, 4, 3, 2, 1, 0, 0], 'realVal':[0,1,3,2,4,3,1,2,1,0]}
-df=pd.DataFrame(data)
-val=Validation(df=df)
-val.calcR2_score(realValCol='realVal', forecastCol='forecast')
-print(val.df)
+
+if __name__ == "__main__":
+    data = {'forecast': [0, 1, 2, 3, 4, 3, 2, 1, 0, 0], 'realVal': [0, 10, -3, 2, -4, 30, 1, 2, 1, 0]}
+    df_test = pd.DataFrame(data)
+    val = Validation(df=df_test)
+    val.calcR2_score(realValCol='realVal', forecastCol='forecast')
+    print(val.df)
---
+++ data_import.py
@@ -0,0 +1,141 @@
+from os import listdir
+import pandas as pd
+import sqlite3
+from sqlalchemy import create_engine
+from tqdm import tqdm
+import json
+
+engine = create_engine('sqlite://', echo=False)
+
+folders_path = 'C:/Users/bo112/E.ON/Platone HAW groupwork - raw data'
+conn = sqlite3.connect('data/input_data.db')
+c = conn.cursor()
+
+
+def import_baseline():
+    baseline_path = '/eIOT-iONS_baseline/PTEI_Baseline_1_Minutes_Mean_2022.csv'
+    df_baseline = pd.read_csv(f'{folders_path}{baseline_path}', sep=';', decimal=',', skiprows=[1])
+    df_baseline['Timestamp'] = pd.to_datetime(df_baseline['Timestamp'], format='%Y-%m-%d %H:%M:%S')
+    df_baseline.set_index('Timestamp', inplace=True)
+    df_baseline.rename(columns={'PTEI - Baseline (Calculated) - 1-Minutes Mean': 'Baseline in kW'}, inplace=True)
+    df_baseline.to_sql('baseline', con=conn)
+
+
+def import_slp():
+    slp_path = '/standard-load-profile/tshouseholdUTC2022.csv'
+    df_slp = pd.read_csv(f'{folders_path}{slp_path}', sep=',', decimal='.')
+    df_slp['Timestamp'] = pd.to_datetime(df_slp['RowKey'].str[:-6], format='%Y-%m-%d %H:%M:%S')
+    df_slp.drop(columns=['RowKey'], inplace=True)
+    df_slp.set_index('Timestamp', inplace=True)
+    df_slp.to_sql('slp', con=conn)
+
+
+def import_households():
+    folder = 'household-batteries'
+    files = [f'{folders_path}/{folder}/{f}' for f in listdir(f'{folders_path}/{folder}')]
+    df_table = pd.DataFrame(columns=['Timestamp'])
+    for file in files:
+        df = pd.read_csv(file, sep=',', decimal='.', skiprows=1)
+        df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%Y-%m-%d %H:%M:%S')
+        df_table = pd.merge(df_table, df, on='Timestamp', how='outer')
+    # only use dates with data for all households
+    df_table.dropna(inplace=True)
+    df_table.set_index('Timestamp', inplace=True)
+    df_table.to_sql('household_batteries', con=conn)
+
+
+def import_mb_basic():
+    folder = 'MB-basic'
+    files = [f'{folders_path}/{folder}/{f}' for f in listdir(f'{folders_path}/{folder}')]
+    df_table = pd.DataFrame()
+    for file in tqdm(files):
+        date = file.split(sep='basic_')[1][:10]
+        df = pd.read_csv(file, sep=',', decimal='.')
+        df = df.loc[df['time'].str.contains(date)]
+        df['Timestamp'] = pd.to_datetime(df['time'].str[:16], format='%Y-%m-%dT%H:%M')
+        df.drop(columns=['time'], inplace=True)
+        df.set_index('Timestamp', inplace=True)
+        df_table = pd.concat([df_table, df])
+    df_table.to_sql('mb_basic', con=conn)
+
+
+def import_mb_clouds():
+    folder = 'MB-clouds'
+    files = [f'{folders_path}/{folder}/{f}' for f in listdir(f'{folders_path}/{folder}')]
+    df_table = pd.DataFrame()
+    for file in tqdm(files):
+        date = file.split(sep='clouds_')[1][:10]
+        df = pd.read_csv(file, sep=',', decimal='.')
+        df = df.loc[df['time'].str.contains(date)]
+        df['Timestamp'] = pd.to_datetime(df['time'].str[:16], format='%Y-%m-%dT%H:%M')
+        df.drop(columns=['time'], inplace=True)
+        df.set_index('Timestamp', inplace=True)
+        df_table = pd.concat([df_table, df])
+    df_table.to_sql('mb_clouds', con=conn)
+
+
+def import_mb_solar():
+    folder = 'MB-solar'
+    files = [f'{folders_path}/{folder}/{f}' for f in listdir(f'{folders_path}/{folder}')]
+    df_table = pd.DataFrame()
+    for file in tqdm(files):
+        date = file.split(sep='solar_')[1][:10]
+        df = pd.read_csv(file, sep=',', decimal='.')
+        df = df.loc[df['time'].str.contains(date)]
+        df['Timestamp'] = pd.to_datetime(df['time'].str[:16], format='%Y-%m-%dT%H:%M')
+        df.drop(columns=['time'], inplace=True)
+        df.set_index('Timestamp', inplace=True)
+        df_table = pd.concat([df_table, df])
+    df_table.to_sql('mb_solar', con=conn)
+
+
+def import_mb_sunmoon():
+    folder = 'MB-sunmoon'
+    files = [f'{folders_path}/{folder}/{f}' for f in listdir(f'{folders_path}/{folder}')]
+    df_table = pd.DataFrame()
+    for file in tqdm(files):
+        date = file.split(sep='sunmoon_')[1][:10]
+        df = pd.read_csv(file, sep=',', decimal='.')
+        df = df.loc[df['time'].str.contains(date)]
+        df['Timestamp'] = pd.to_datetime(df['time'].str[:16], format='%Y-%m-%dT%H:%M')
+        df.drop(columns=['time'], inplace=True)
+        df.set_index('Timestamp', inplace=True)
+        df_table = pd.concat([df_table, df])
+    df_table.to_sql('mb_sunmoon', con=conn)
+
+
+def import_mb_pvpro4():
+    folder = 'MB-pvpro_4'
+    files = [f'{folders_path}/{folder}/{f}' for f in listdir(f'{folders_path}/{folder}')]
+    df_table = pd.DataFrame()
+    for file in tqdm(files):
+        date = file.split(sep='PVpro_4_')[1][:10]
+        with open(file) as f:
+            data = json.load(f)
+        df = pd.DataFrame(data.get('data_1h', data.get('data_xmin')))
+        df = df.loc[df['time'].str.contains(date)]
+        df['Timestamp'] = pd.to_datetime(df['time'].str[:16], format='%Y-%m-%dT%H:%M')
+        df.drop(columns=['time'], inplace=True)
+        df.set_index('Timestamp', inplace=True)
+        df_table = pd.concat([df_table, df])
+    df_table.to_sql('mb_pvpro_1h', con=conn)
+
+
+# TODO: pvpro_5 doppelte daten?
+def import_mb_pvpro5():
+    folder = 'MB-pvpro_5'
+    files = [f'{folders_path}/{folder}/{f}' for f in listdir(f'{folders_path}/{folder}')]
+    df_table = pd.DataFrame()
+    for file in tqdm(files):
+        date = file.split(sep='PVpro_5_')[1][:10]
+        with open(file) as f:
+            data = json.load(f)
+        df = pd.DataFrame(data.get('data_xmin'))
+        df = df.loc[df['time'].str.contains(date)]
+        df['Timestamp'] = pd.to_datetime(df['time'].str[:16], format='%Y-%m-%dT%H:%M')
+        df.drop(columns=['time'], inplace=True)
+        df.set_index('Timestamp', inplace=True)
+        df_table = pd.concat([df_table, df])
+    df_table.to_sql('mb_pvpro_15min', con=conn)
+
+
